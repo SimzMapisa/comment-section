1b3806d21bdb360e4def35b8cbb15bb7f481567f
SimzMapisa <simbarashe795@gmail.com>
2022-11-28 12:55:59 +0000

    import dummy data from data.json file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Comment from './components/comment/comment';
 
+import data from './data.json';
+
 const App = () => {
 	return (
 		<div className='container'>
-			<Comment />
+			<Comment data={data} />
 		</div>
 	);
 };
